b77a8a3ebfe16dd2e171d6c32b1d5dbb1fe37ab7
Lucaziki <Lucaziki@users.noreply.github.com>
2023-07-01 21:32:36 +0800

    调整设置以适应本地Flutter环境

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,10 +6,10 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import screen_retriever
 import share_plus
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import window_manager
